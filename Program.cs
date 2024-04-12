63749c228056ae97c26f9ba301721a1e0dff3b90
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-12 15:32:30 +0200

    returning values from methods

--- Program.cs
+++ Program.cs
@@ -4,19 +4,29 @@ double minimumSpend = 30.00;
 double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
 double[] discounts = { 0.30, 0.00, 0.10, 0.20, 0.50 };
 
-Console.WriteLine($"Total: ${total}");
-
-void GetDiscountedPrice(int itemIndex)
+for (int i = 0; i < items.Length; i++)
 {
-    double result = items[itemIndex] * (1 - discounts[itemIndex]);
+    total += GetDiscountedPrice(i);
 }
 
-void TotalMeetsMinimum()
+if (TotalMeetsMinimum())
 {
-    // Check if the total meets the minimum
+    total -= 5.00;
 }
 
-void FormatDecimal(double input)
+Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+double GetDiscountedPrice(int itemIndex)
 {
-    // Format the double so only 2 decimal places are displayed
+    return items[itemIndex] * (1 - discounts[itemIndex]);
+}
+
+bool TotalMeetsMinimum()
+{
+    return total >= minimumSpend;
+}
+
+string FormatDecimal(double input)
+{
+    return input.ToString().Substring(0, 5);
 }
