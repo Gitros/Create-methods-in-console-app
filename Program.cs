bfb729f0c836081c2d01bb8d6f0dae40c75a048b
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-12 11:26:50 +0200

    method implementing

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int a = 3;
+int b = 4;
+int c = 0;
+
+Multiply(a, b, c);
+Console.WriteLine($"global statement: {a} x {b}, = {c}");
+
+void Multiply(int a, int b, int c)
+{
+    c = a * b;
+    Console.WriteLine($"Inside Multiply method {a} x {b} = {c}");
+}
