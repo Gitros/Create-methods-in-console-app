ce95ac2f89f585ca28bde1a8f96e8d73c63fae71
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-12 16:54:12 +0200

    reverse words

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿string input = "snake";
+Console.WriteLine(input);
+Console.WriteLine(ReverseWord(input));
+
+string ReverseWord(string word)
+{
+    string result = "";
+    for (int i = word.Length - 1; i >= 0; i--)
+    {
+        result += word[i];
+    }
+
+    return result;
+}
