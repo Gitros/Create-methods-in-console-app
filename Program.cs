a1f53978a2bbc7c7d34bb1e4ec7156453a880eda
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-12 11:11:31 +0200

    clear

--- Program.cs
+++ Program.cs
@@ -1,13 +1,1 @@
-﻿string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
-
-DisplayStudents(students);
-DisplayStudents(new string[] { "Robert", "Vanya" });
-
-void DisplayStudents(string[] students)
-{
-    foreach (string student in students)
-    {
-        Console.Write($"{student}, ");
-    }
-    Console.WriteLine();
-}
+﻿
