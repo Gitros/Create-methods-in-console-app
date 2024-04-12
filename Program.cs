655e66ec0f5f7351223149ed51c034aed2981408
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-12 16:54:21 +0200

    clear

--- Program.cs
+++ Program.cs
@@ -1,14 +1,1 @@
-﻿string input = "snake";
-Console.WriteLine(input);
-Console.WriteLine(ReverseWord(input));
-
-string ReverseWord(string word)
-{
-    string result = "";
-    for (int i = word.Length - 1; i >= 0; i--)
-    {
-        result += word[i];
-    }
-
-    return result;
-}
+﻿
