a6f2d6845db5ecca1b79458e5c277d19217076b9
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-13 11:41:25 +0200

    roll dice game with methods

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿Random random = new Random();
 
 Console.WriteLine("Would you like to play? (Y/N)");
+
 if (ShouldPlay())
 {
     PlayGame();
@@ -12,14 +13,29 @@ void PlayGame()
 
     while (play)
     {
-        var target;
-        var roll;
+        var target = random.Next(1, 6);
+        var roll = random.Next(1, 7);
 
         Console.WriteLine($"Roll a number greater than {target} to win!");
         Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose());
+        Console.WriteLine(WinOrLose(target, roll));
         Console.WriteLine("\nPlay again? (Y/N)");
 
         play = ShouldPlay();
     }
+}
+
+bool ShouldPlay()
+{
+    string response = Console.ReadLine();
+    return response.ToLower().Equals("y");
+}
+
+string WinOrLose(int target, int roll)
+{
+    if (roll > target)
+    {
+        return "You win!";
+    }
+    return "You loose.";
 }
